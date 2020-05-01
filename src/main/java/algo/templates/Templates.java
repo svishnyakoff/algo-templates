49a84af6c7991f98322b6c19b19a5ffc072eb45e
sergey.vishnyakov <sergey.vishnyakoff@gmail.com>
2020-05-01 18:04:14 -0400

    MST Prims algorithm

--- src/main/java/algo/templates/Templates.java
+++ src/main/java/algo/templates/Templates.java
@@ -1,9 +1,48 @@
 package algo.templates;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class Templates {
+
+    public static Set<Edge> mstPrims(int n, Map<Integer, List<Edge>> graph) {
+
+        // contains vertices that already added to mst
+        Set<Integer> mst = new HashSet<>();
+        Set<Edge> mstEdges = new HashSet<>();
+
+        PriorityQueue<Edge> q = new PriorityQueue<>(Comparator.comparingInt(edge -> edge.weight));
+
+        // let start from random vertex
+        int src = 0;
+
+        // add vertex we want to start from to mst and add all adjacent edges to queue.
+        mst.add(src);
+        q.addAll(graph.get(src));
+
+        // during each iteration peek the edge with smallest weight pointing to node outside of MST
+        while(!q.isEmpty()) {
+            Edge edge = q.poll();
+            int v = edge.v;
+
+            // skip edges that are pointing to node that is already in MST
+            if (mst.contains(v)) continue;
+
+            mst.add(v);
+            mstEdges.add(edge);
+
+            for (Edge e : graph.get(v)) {
+                if (!mst.contains(e.v)) q.add(e);
+            }
+        }
+
+        return mstEdges;
+    }
 
     /**
      * Preorder traversal of the graph using Morris traversal
@@ -39,6 +78,16 @@ public class Templates {
         }
 
         return result;
+    }
+
+    static class Edge {
+        int u, v, weight;
+
+        public Edge(int u, int v, int weight) {
+            this.u = u;
+            this.v = v;
+            this.weight = weight;
+        }
     }
 
     public class TreeNode {
